ed3ec81e0b5b71f14f1fd7a7acefbf146f628d10
Dennis Wenrich <dennisfinley@gmail.com>
2021-10-20 22:00:50 -0400

    moved app contents to 12 file, installed nodemon

--- 12-http.js
+++ 12-http.js
@@ -1,6 +1,13 @@
-const _ = require('lodash')
+const http = require('http');
 
-const items = [1, [2, [3, [4]]]]
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.end('Welcome To Our Homepage')
+    } else if (req.url === '/about') {
+        res.end('Here is our short history')
+    } else {
+        res.end(`<h1>Oops!</h1><p>We can't seem to find the page that you are looking for</p><a href="/">Back Home</a>`)
+    }
+})
 
-const newItems = _.flattenDeep(items)
-console.log(newItems);
+server.listen(5000)
--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-const http = require('http');
+const _ = require('lodash')
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.end('Welcome To Our Homepage')
-    } else if (req.url === '/about') {
-        res.end('Here is our short history')
-    } else {
-        res.end(`<h1>Oops!</h1><p>We can't seem to find the page that you are looking for</p><a href="/">Back Home</a>`)
-    }
-})
+const items = [1, [2, [3, [4]]]]
 
-server.listen(5000)
+const newItems = _.flattenDeep(items)
+console.log(newItems);
+
+console.log('Hello People!')
